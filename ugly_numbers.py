7d1f41b6797720d0e59b2a4d266e717348619889
Kevin Mercurio <kevin.mercurio@gmail.com>
2016-03-21 21:31:08 -0400

    ugly numbers

---
+++ ugly_numbers.py
@@ -0,0 +1,35 @@
+"""
+Write a program to check whether a given number is an ugly number.
+
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7.
+
+Note that 1 is typically treated as an ugly number.
+
+>>> test_ugly(7)
+False
+
+"""
+
+
+def test_ugly(num):
+    """if 1, return True, if zero or negative, return False"""
+
+    if num == 1:
+        return True
+    if num <= 0:
+        return False
+
+    while num > 1:
+        if num%2 == 0:
+            num /= 2
+        elif num%3 == 0:
+            num /= 3
+        elif num%5 == 0:
+            num /= 5
+        else:
+            return False
+        return True
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
